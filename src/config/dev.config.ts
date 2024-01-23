1df817cda1369444d2e0d10b19fc5cfd0afb58e1
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-23 09:19:28 +0200

    + add mongoURI to dev config

--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -3,9 +3,10 @@ import { IConfig } from './keys.config';
 
 dotenv.config({ path: `${process.cwd()}/dev.env` });
 
-const { NODE_ENV, PORT } = process.env;
+const { NODE_ENV, PORT, MONGO_URI } = process.env;
 
 export default {
 	env: NODE_ENV || 'development',
 	port: PORT || 3001,
+	mongoURI: MONGO_URI || 'mongodb://localhost:27017/app',
 } as IConfig;
