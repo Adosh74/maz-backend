db0a5ef618b64cb874419ead05cd018fcd24d8bb
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-26 09:27:14 +0200

    + add email dev credentials

--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -3,10 +3,24 @@ import { IConfig } from './keys.config';
 
 dotenv.config({ path: `${process.cwd()}/dev.env` });
 
-const { NODE_ENV, PORT, MONGO_URI } = process.env;
+const {
+	NODE_ENV,
+	PORT,
+	MONGO_URI,
+	SENDINBLUE_EMAIL,
+	SENDINBLUE_HOST,
+	SENDINBLUE_PORT,
+	SENDINBLUE_USERNAME,
+	SENDINBLUE_PASSWORD,
+} = process.env;
 
 export default {
 	env: NODE_ENV || 'development',
 	port: PORT || 3001,
 	mongoURI: MONGO_URI || 'mongodb://localhost:27017/app',
+	sendInBlueEmail: SENDINBLUE_EMAIL || '',
+	sendInBlueHost: SENDINBLUE_HOST || '',
+	sendInBluePort: SENDINBLUE_PORT || 587,
+	sendInBlueUsername: SENDINBLUE_USERNAME || '',
+	sendInBluePassword: SENDINBLUE_PASSWORD || '',
 } as IConfig;
