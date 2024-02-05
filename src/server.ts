acf2f21edad1204cf49093b77154cc8e795de534
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-05 14:53:01 +0200

    + update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
+import colors from 'colors';
 import { app } from './app';
 import connectDB from './config/database.config';
+import config from './config/keys.config';
 
 connectDB();
 
 app.listen(3001, () => {
-	console.log('Server started on port 3001');
+	console.log(colors.bgGreen(`Server running on port ${config.port}`));
 });
