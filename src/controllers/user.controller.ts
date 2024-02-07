3e7ba4a3557568ac02215794aeef64f5a3dcac1a
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-07 22:15:10 +0200

    + add getMe into user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,3 +9,9 @@ export const getOneUser = Factory.getOne(User);
 export const createUser = Factory.createOne(User);
 export const updateUser = Factory.updateOne(User);
 export const deleteUser = Factory.deleteOne(User);
+
+// *** getMe - get current user
+export const getMe = (req: any, res: any, next: any) => {
+	req.params.id = req.user.id;
+	next();
+};
