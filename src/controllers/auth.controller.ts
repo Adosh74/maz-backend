25dea7ef66354cf5d8c9d9ce7b9cd2359f54d46c
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-07 22:14:28 +0200

    + add protect for middleware

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,8 @@
 import crypto from 'crypto';
 import ejs from 'ejs';
 import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import path from 'path';
-import { promisify } from 'util';
 import config from '../config/keys.config';
 import User, { IUserSchema } from '../models/user.model';
 import AppError from '../utils/AppError.util';
@@ -109,5 +108,67 @@ export const login = catchAsync(
 
 		// +[3] Send token
 		createSendToken(user, 200, res);
+	}
+);
+
+// *** Protect routes
+export const protect = catchAsync(
+	async (req: Request, res: Response, next: NextFunction) => {
+		// +[1] Get token and check if it's there
+		let token: string | undefined;
+		if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+			token = req.headers.authorization.split(' ')[1];
+		} else if (req.cookies.jwt) {
+			token = req.cookies.jwt;
+		}
+
+		// +[2] Check if token not exists
+		if (!token) {
+			return next(
+				new AppError('You are not logged in! Please log in to get access', 401)
+			);
+		}
+
+		// +[3] Verify token
+		const promisify = (fn: any) => {
+			return (...args: any) => {
+				return new Promise((resolve, reject) => {
+					fn(...args, (err: any, data: any) => {
+						if (err) return reject(err);
+						resolve(data);
+					});
+				});
+			};
+		};
+
+		const decoded: any = (await promisify(jwt.verify)(
+			token,
+			config.jwtSecret
+		)) as JwtPayload;
+
+		// +[4] Check if user still exists
+		const currentUser: IUserSchema | null = await User.findById(decoded.id);
+
+		// +[5] Check if user still exists
+		if (!currentUser) {
+			return next(
+				new AppError('The user belonging to this token does no longer exist', 401)
+			);
+		}
+
+		// +[6] Check if user changed password after the token was issued
+		if (
+			currentUser.passwordChangedAt &&
+			decoded.iat < currentUser.passwordChangedAt.getTime() / 1000
+		) {
+			return next(
+				new AppError('User recently changed password! Please log in again', 401)
+			);
+		}
+
+		// +[7] Grant access to protected route
+		(req as any).user = currentUser;
+
+		next();
 	}
 );
