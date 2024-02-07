51095dffd23bb6169efc92c83e80ecbdb0074200
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-07 21:01:50 +0200

    + produce jwt credentials + produce smtp credentials

--- src/config/keys.config.ts
+++ src/config/keys.config.ts
@@ -4,15 +4,21 @@ export interface IConfig {
 	env: string;
 	port: number;
 	mongoURI?: string;
-	sendInBlueEmail?: string;
-	sendInBlueHost?: string;
-	sendInBluePort?: number;
-	sendInBlueUsername?: string;
-	sendInBluePassword?: string;
+	smtpHost?: string;
+	smtpPort?: number;
+	smtpService?: string;
+	smtpUser?: string;
+	smtpPass?: string;
+	jwtSecret: string;
+	jwtExpiresIn: string;
+	jwtCookieExpiresIn: number;
 }
 let config: IConfig = {
 	env: 'development',
 	port: 3001,
+	jwtSecret: 'my-secret',
+	jwtExpiresIn: '5d',
+	jwtCookieExpiresIn: 5,
 };
 
 if (process.env.NODE_ENV === 'development') {
