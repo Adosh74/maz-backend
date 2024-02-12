d57a9cfacb45defe4f865b5e7b62f02c52022c13
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-12 02:47:46 +0200

    + update server

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
 import colors from 'colors';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { app } from './app';
 // import connectDB from './config/database.config';
 import config from './config/keys.config';
 import AppError from './utils/AppError.util';
 
+dotenv.config({ path: `${process.cwd()}/dev.env` });
+
 // connectDB();
 
 if (!config.mongoURI) new AppError('MongoURI not found', 500);
 mongoose
-	.connect(
-		'mongodb+srv://mazrealty:bXk41XT1QpF0Ym3o@mazrealty.0u1azjg.mongodb.net/mazrealty',
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-		}
-	)
+	.connect(process.env.MONGO_URI || '', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
 	.then((data) => {
 		console.log(colors.bgMagenta(`${data.connection.name} 'connected to MongoDB`));
 
