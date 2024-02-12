b2604452e81c81f2f524d7abbedf610552f36343
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-12 02:30:02 +0200

    + update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import colors from 'colors';
+import mongoose from 'mongoose';
 import { app } from './app';
-import connectDB from './config/database.config';
+// import connectDB from './config/database.config';
 import config from './config/keys.config';
+import AppError from './utils/AppError.util';
 
-connectDB();
+// connectDB();
 
-app.listen(3001, () => {
-	console.log(colors.bgGreen(`Server running on port ${config.port}`));
-});
+if (!config.mongoURI) new AppError('MongoURI not found', 500);
+mongoose
+	.connect(config.mongoURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then((data) => {
+		console.log(colors.bgMagenta(`${data.connection.name} 'connected to MongoDB`));
+
+		app.listen(3001, () => {
+			console.log(colors.bgGreen(`Server running on port ${config.port}`));
+		});
+	})
+	.catch((err) => console.log(err));
