f0eb5fe06f790d02598d50d1de16393e1c1fa3da
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-19 15:03:32 +0200

    + add crud operation for property

--- src/controllers/property.controller.ts
+++ src/controllers/property.controller.ts
@@ -1,2 +1,10 @@
 import Property from '../models/property.model';
 import * as Factory from './handlerFactory.controller';
+
+// *** CRUD operation for property
+
+export const getAllProperty = Factory.getAll(Property);
+export const getOneProperty = Factory.getOne(Property, 'owner');
+export const createOneProperty = Factory.createOne(Property);
+export const updateOneProperty = Factory.updateOne(Property);
+export const deleteOneProperty = Factory.deleteOne(Property);
