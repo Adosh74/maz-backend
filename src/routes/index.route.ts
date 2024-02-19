367555db87046699fd81217bb088460fbaea910a
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-19 15:04:17 +0200

    + add properties for route APIs

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import authRoutes from './APIs/auth.route';
+import propertyRoutes from './APIs/property.route';
 import userRoutes from './APIs/user.route';
 
 const routes = Router();
@@ -8,5 +9,6 @@ const routes = Router();
 
 routes.use('/users', userRoutes);
 routes.use('/auth', authRoutes);
+routes.use('/properties', propertyRoutes);
 
 export default routes;
