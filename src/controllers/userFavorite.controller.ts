6c41fd89789a265c6eb2656aa34fb78c0e8d26a3
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-03-01 17:15:11 +0200

    + add userFavorite handler file + add createFavorite handler + add getFavorites handler

---
+++ src/controllers/userFavorite.controller.ts
@@ -0,0 +1,57 @@
+import { Request, Response, NextFunction } from 'express';
+import Property from '../models/property.model';
+import UserFavorite from '../models/userFavorite.model';
+import AppError from '../utils/AppError.util';
+import catchAsync from '../utils/catchAsync.util';
+
+// *** createFavorite ***
+export const createFavorite = catchAsync(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const { propertyId } = req.params;
+		const userId = (req as any).user._id;
+
+		// +[1] check if property exists
+		const property = await Property.findById(propertyId);
+
+		if (!property) {
+			return next(new AppError('Property not found', 404));
+		}
+		// +[2] check if user has already favorited the property
+		const favorite = await UserFavorite.findOne({
+			user: userId,
+			property: propertyId,
+		});
+
+		if (favorite) {
+			return next(new AppError('Property already favorited', 400));
+		}
+
+		// +[3] create favorite
+		const newFavorite = await UserFavorite.create({
+			user: userId,
+			property: propertyId,
+		});
+
+		res.status(201).json({
+			status: 'success',
+			data: {
+				favorite: newFavorite,
+			},
+		});
+	}
+);
+
+// *** get all user favorites and populate property ***
+export const getFavorites = catchAsync(async (req: Request, res: Response) => {
+	const userId = (req as any).user._id;
+
+	const favorites = await UserFavorite.findUserFavorites(userId);
+
+	res.status(200).json({
+		status: 'success',
+		results: favorites.length,
+		data: {
+			favorites,
+		},
+	});
+});
