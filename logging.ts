fe8b923ed80358bb0e2ea84b25851aae23c866cd
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-03-01 17:31:55 +0200

    + add logging module

---
+++ logging.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { pino } from 'pino';
+import pretty from 'pino-pretty';
+import config from './src/config/keys.config';
+
+const streams: { write: any }[] = [
+	config.env === 'production' ? process.stdout : pretty(),
+	fs.createWriteStream(path.join(__dirname, 'process.log')),
+];
+
+export const LOGGER = pino(
+	{
+		redact: ['body.password'],
+		formatters: {
+			bindings: () => ({}),
+		},
+	},
+	pino.multistream(streams)
+);
