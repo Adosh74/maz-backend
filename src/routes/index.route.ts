b7d638f3ed1a68dc7cbf1d94f0fde3255e2ffa9c
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-03-02 22:43:43 +0200

    + add favorites endpoint into main api

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import authRoutes from './APIs/auth.route';
 import propertyRoutes from './APIs/property.route';
 import userRoutes from './APIs/user.route';
+import userFavoriteRoutes from './APIs/userFavorite.route';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ const routes = Router();
 routes.use('/users', userRoutes);
 routes.use('/auth', authRoutes);
 routes.use('/properties', propertyRoutes);
+routes.use('/favorites', userFavoriteRoutes);
 
 export default routes;
