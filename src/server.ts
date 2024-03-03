16fc1ad362a1f2ec3efec5442e8a28cafe23cbcf
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-03-03 18:28:20 +0200

    + clean server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,20 @@
 import dotenv from 'dotenv';
-import Redis from 'ioredis';
-import mongoose from 'mongoose';
 import { app as server } from './app';
+import './database/mongo';
+import './database/redis';
 import { LOGGER } from './logging';
 
 dotenv.config({ path: `${process.cwd()}/.env` });
 
-const { PORT, MONGO_URI, NODE_ENV, REDIS_URL } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
-if (!MONGO_URI || !PORT || !REDIS_URL) {
+if (!PORT) {
 	LOGGER.error(
-		'Please make sure that you have a valid mongoURI and port number and redis url in your .env file'
+		'Please make sure that you have a valid mongoURI and port number in your .env file'
 	);
 	process.exit(1);
 }
 
-// *** Redis Connection
-export const redisClient = new Redis(REDIS_URL);
-
-redisClient.on('connect', () => {
-	LOGGER.info('Connected to Redis');
+server.listen(PORT, () => {
+	LOGGER.info(`Server running in ${NODE_ENV} mode on port ${PORT}`);
 });
-
-redisClient.on('error', (err) => {
-	LOGGER.error(`Redis error: ${err}`);
-	process.exit(1);
-});
-
-// *** MongoDB Connection and Server Start
-mongoose
-	.connect(MONGO_URI || '', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-	})
-	.then((data) => {
-		LOGGER.info(`Connected to MongoDB ${data.connection.name} database`);
-		server.listen(PORT, () =>
-			LOGGER.info(`Listening on port ${PORT} in ${NODE_ENV} environment`)
-		);
-	})
-	.catch((err) => {
-		LOGGER.error(err);
-		process.exit(1);
-	});
