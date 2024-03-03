6b84d7120fc07ce5cb7c5be6e3f340a528ca5e04
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-03-03 18:27:35 +0200

    + split mongo connection into specific module

---
+++ src/database/mongo.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { LOGGER } from '../logging';
+
+dotenv.config({ path: `${process.cwd()}/.env` });
+
+const { MONGO_URI } = process.env;
+
+if (!MONGO_URI) {
+	LOGGER.error('Please make sure that you have a valid mongoURI in your .env file');
+	process.exit(1);
+}
+
+// *** MongoDB Connection
+mongoose
+	.connect(MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then((data) => LOGGER.info(`Connected to MongoDB ${data.connection.name} database`))
+	.catch((err) => LOGGER.error(err));
