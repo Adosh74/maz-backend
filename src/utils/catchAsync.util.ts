966e55807f884796654552b4b014862ea40c0b62
Mohamed Shebl <hamdysheblm@gmail.com>
2024-04-12 21:58:31 +0200

    + add AsyncRequestHandler type for catchAsync wrapper

--- src/utils/catchAsync.util.ts
+++ src/utils/catchAsync.util.ts
@@ -1,6 +1,12 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
-export default (fn: Function) => {
+type AsyncRequestHandler = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => Promise<any>;
+
+export default (fn: AsyncRequestHandler): RequestHandler => {
 	return (req: Request, res: Response, next: NextFunction) => {
 		Promise.resolve(fn(req, res, next)).catch(next);
 	};
