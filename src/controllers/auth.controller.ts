cdd67e07fb37ed877c91af45f719e6b0b5c48371
Mohamed Shebl <hamdysheblm@gmail.com>
2024-04-22 21:24:05 +0200

    + add logout endpoint and functionality

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -211,3 +211,13 @@ export const updatePassword = catchAsync(
 		createSendToken(user, 200, res);
 	}
 );
+
+// *** logout
+export const logout = (req: Request, res: Response) => {
+	res.cookie('jwt', 'loggedout', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+
+	res.status(200).json({ status: 'success' });
+};
